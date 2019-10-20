ed451a1bef505507c2ada6e026a7c290e9f2a686
salahdin <salahdinjemal@gmail.com>
2019-10-20 19:00:15 +0200

    first commit

---
+++ NetBeansProjects/Lab6-Student/src/conn.java
@@ -0,0 +1,26 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 201404824
+ */
+class conn {
+    static PreparedStatement prepareStatement(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
